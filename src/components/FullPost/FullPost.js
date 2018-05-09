5aed8b11179017540323abca0e575021b678dca6
jujuck <julien.y.richard@gmail.com>
2018-05-09 16:45:57 +0200

    fin du module 9, request Ajax

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,21 +1,54 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './FullPost.css';
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
+    state = {
+        loadedPosts: null
+    }
 
-        );
+    componentDidUpdate() {
+        if (this.props.id) {
+            if (!this.state.loadedPosts || (this.state.loadedPosts && this.state.loadedPosts.id !== this.props.id)) {
+                axios.get('posts/' + this.props.id)
+                .then(response => {
+                    this.setState({loadedPosts : response.data})
+                });
+            }
+        }
+    }
+
+    deletePostHandler = () => {
+        axios.delete('posts/' + this.props.id)
+            .then(response => {
+                console.log(response);
+            });
+    }
+
+
+    render () {
+        let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
+        if (this.props.id) {
+            post = <p style={{textAlign: 'center'}}>Chargement du post ...</p>;
+            console.log("loading")
+        }
+        if (this.state.loadedPosts) {
+            post = (
+                <div className="FullPost">
+                    <h1>{this.state.loadedPosts.title}</h1>
+                    <p>{this.state.loadedPosts.body}</p>
+                    <div className="Edit">
+                        <button 
+                            className="Delete"
+                            onClick={this.deletePostHandler}>Delete</button>
+                    </div>
+                </div>
+    
+            );
+        }
         return post;
+       
     }
 }
 
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,18 +4,57 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from 'axios';
 
 class Blog extends Component {
+    state = {
+        posts: [],
+        selectedPostId: null,
+        error: false
+    }
+
+    componentDidMount () {
+        axios.get('posts')
+        .then(response => {
+            const posts = response.data.slice(0, 4);
+            const updatedPosts = posts.map(post => {
+                return {
+                    ...post,
+                    author: 'Max'
+                }
+            });
+            this.setState({posts: updatedPosts});
+        })
+        .catch(error => {
+            this.setState(error : true)
+        });
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id})
+
+    }
+
+
     render () {
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong </p>
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post 
+                            title={post.title} 
+                            author={post.author}
+                            key={post.id} 
+                            clicked={() => this.postSelectedHandler(post.id)}/>
+            } );
+        }
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
@@ -26,3 +65,4 @@ class Blog extends Component {
 }
 
 export default Blog;
+
